e903ed167554f2a817d9fafb35b5d0cc87962ad3
Amaranth01 <vanessa.nectoux@gmail.com>
2022-01-12 15:37:48 +0100

    Exo 152 ok

--- index.php
+++ index.php
@@ -1,9 +1,15 @@
 <?php
 
 // Premiere ligne
+$premiereDate = date("D M J Y");
+echo $premiereDate;
 
+echo "<br><br>";
 
+$past = strtotime("2018.12.10");
+echo date('Y.m.d', $past);
 
+echo "<br><br>";
 
-
-
+$hour = strtotime("11:35:07");
+echo date('H:i:s', $hour);
